788948435771061ed44f0dcee5e77f0997a4cae0
Çağın Yarkın Sarı <cyarkinsari@gmail.com>
2022-08-31 16:54:20 +0300

    get-post for main route

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const ejs = require("ejs");
 //app setups
 const port = 3000;
 const app = express();
+
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended: true}));
-app.use(express.json);
+app.use(express.json());
 app.use(express.static("public"));
+
 //db setup
 mongoose.connect("mongodb://localhost:27017/wikiDB");
 
@@ -20,10 +23,37 @@ const articleSchema = {
 
 const ArticleModel = mongoose.model("article", articleSchema);
 
+//get /articles
+app.get("/articles", (req, res) => {
+    ArticleModel.find((err, articles)=>{
+        if (!err) {
+            res.send(articles);
+        }else{
+            res.send(err);
+        };
+    });
+   
+});
+//post /articles
+app.post("/articles", (req, res)=>{
+
+    const newArticle = new ArticleModel({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save((err)=>{
+        if (!err) {
+            res.send("Succesfully added a new article.")
+        }else{
+            res.send(err);
+        };
+    });
+});
 
 
 
 //Listen
 app.listen(port, () => {
-    console.log("Server is up port: 3000")
+    console.log("Server is up port: 3000");
 });
